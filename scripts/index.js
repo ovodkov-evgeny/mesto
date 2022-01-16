16613314fc36ecca436967f0b7bda437254129f5
Evgeny Ovodkov <kefir4ek@gmail.com>
2022-01-16 23:28:15 +0300

    Post-review fixes 2.0

--- scripts/index.js
+++ scripts/index.js
@@ -2,20 +2,21 @@ let editBtn = document.querySelector('.profile__btn-edit');
 let closeBtn = document.querySelector('.popup__btn-close');
 let profileName = document.querySelector('.profile__name');
 let profileText = document.querySelector('.profile__text');
-let form = document.querySelector('.popup');
-let nameInput = form.querySelector('.edit-form__input[name="name"]');
-let aboutInput = form.querySelector('.edit-form__input[name="about"]');
+let popup = document.querySelector('.popup');
+let editForm = document.querySelector('.edit-form');
+let nameInput = editForm.querySelector('.edit-form__input[name="name"]');
+let aboutInput = editForm.querySelector('.edit-form__input[name="about"]');
 
 function formOpenHandler(evt) {
 	evt.preventDefault();
-	form.classList.add('popup_opened');
+	popup.classList.add('popup_opened');
 	nameInput.value = profileName.textContent;
 	aboutInput.value = profileText.textContent;
 }
 
 function formCloseHandler(evt) {
 	evt.preventDefault();
-	form.classList.remove('popup_opened');
+	popup.classList.remove('popup_opened');
 }
 
 function formSubmitHandler (evt) {
@@ -30,4 +31,4 @@ function formSubmitHandler (evt) {
 
 editBtn.addEventListener('click', formOpenHandler);
 closeBtn.addEventListener('click', formCloseHandler);
-form.addEventListener('submit', formSubmitHandler);
+editForm.addEventListener('submit', formSubmitHandler);
